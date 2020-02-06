b814b9b42b2afd0d96c72eb07620df177d4744d8
MelvinManni <melvinnnamani@gmail.com>
2020-02-06 11:31:36 +0100

    Table and mob-nav adjustment

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 //Open&Close menu btn
 const openBtn = document.getElementById("openBtn"),
-    closeBtn = document.getElementById("closeBtn");
+    closeBtn = document.getElementById("closeBtn"),
+    depoTSelect = document.getElementById("depo-ts");
 
 openBtn.addEventListener("click", (e)=>{
     document.getElementById("bg-black").style.display = "block";
@@ -10,3 +11,57 @@ openBtn.addEventListener("click", (e)=>{
 closeBtn.addEventListener("click", (e)=>{
     document.getElementById("bg-black").style.display = "none";
 });
+
+var table = document.getElementById("t-body");
+const x = 50;
+for (let i=0; i<x; i++){
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    `;
+
+    table.appendChild(row);
+}
+
+
+// depoTSelect.addEventListener("change", ()=>{
+//     const x = parseInt(depoTSelect.value);
+//     const table = document.getElementById("depo-table");
+    
+//     const deepRows = document.getElementsByClassName('depo-rows');
+//     for(let y = 0; y < deepRows.length; y++){
+//         deepRows.style.display = 'none'
+//     };
+
+//     for (let i = 0; i< x; i++){
+//         const row = document.createElement("tr");
+//         row.className = 'depo-rows'
+//         row.innerHTML = `
+//         <td></td>
+//         <td></td>
+//         <td></td>
+//         <td></td>
+//         <td></td>
+//         <td></td>
+//         <td></td>
+//         <td></td>
+//     `;
+
+//         table.appendChild(row);
+//     };
+
+
+// });
+
+
+onclickLoad=()=>{
+    
+    
+};
